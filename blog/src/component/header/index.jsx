cbe79df02d4f6067a212b9c4be2d763f130e2429
SeungHyunHan <vkflek1232@naver.com>
2022-08-22 15:34:19 +0900

    헤더완성

--- blog/src/component/header/index.jsx
+++ blog/src/component/header/index.jsx
@@ -3,8 +3,22 @@ import styled from 'styled-components';
 
 const Header = () => {
   return (
-    <div>header</div>
+    <Wrap>
+      <Logo src = {'MainLogo.svg'}/>
+    </Wrap>
   )
 }
+
+const Wrap = styled.div`
+  background-color: #1C3879;
+  width: 100%;
+  height: 100%;
+`
+
+const Logo = styled.img`
+  width: 100%;
+  height: 80%;
+  padding-top: 1%;
+`
 
 export default Header
