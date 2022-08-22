c5ce45cbbce7df57e04e57c57097d21c93b8ec7c
SeungHyunHan <vkflek1232@naver.com>
2022-08-22 16:26:45 +0900

    프로필완성

--- blog/src/component/sideBar/profile/index.jsx
+++ blog/src/component/sideBar/profile/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
-const Index = () => {
+import React from 'react';
+import styled from 'styled-components';
+const Profile = () => {
   return (
-    <div>프로필</div>
+    <Wrap>
+      <ProfileImage src='/profile.jpg' />
+      <ProfileText>개발자 한승현의 블로그입니다.<br/>공부하고 만든것들을 소개하는 사이트입니다.</ProfileText>
+    </Wrap>
   )
 }
 
-export default Index
+const Wrap = styled.div`
+  background-color: #607EAA;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items : center;
+`
+
+const ProfileImage = styled.img`
+  width: 60%;
+  height: 60%;
+  border-radius: 50%;
+  padding: 5%;
+`
+
+const ProfileText = styled.div`
+  color: white;
+  white-space: pre-wrap;
+  text-align: center;
+`
+
+export default Profile;
